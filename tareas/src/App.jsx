1d9b1c7c1a6d3e91877e75b3ce9e972732241192
luz-luna9709 <89154249+luz-luna9709@users.noreply.github.com>
2022-10-19 00:57:35 -0600

    tareas react

---
+++ tareas/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+import Tareas from "./components/Tareas"
+function App() {
+
+  return (
+    <div className ='container'>
+      <Header />
+      <Tareas />
+    
+    </div>
+  )
+}
+
+export default App
---
+++ tareas/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = ({texto,color, onClick}) => {
+   
+    return (
+
+    <button onClick={onClick} style={{backgroundColor:color}} className='btn'>{texto}</button>
+  
+    )
+}
+
+export default Button
